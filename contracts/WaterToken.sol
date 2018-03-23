a366561cbf4995e7c5b8262bad8982a32b8e2366
John Shutt <john.d.shutt@gmail.com>
2018-03-22 18:12:31 -0700

    Create a first draft of a WaterToken contract

---
+++ contracts/WaterToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.21;
+
+import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+
+contract WaterToken is MintableToken {
+  string public name = "Water";
+  string public symbol = "💧";
+  uint public decimals = 2;
+  uint public INITIAL_SUPPLY = 1000;
+
+  function MintableToken() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
