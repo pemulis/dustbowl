9152dc2235c9998c5604d222c6c958a5d4dbbc0c
John Shutt <john.d.shutt@gmail.com>
2018-03-24 17:56:26 -0700

    Make contracts in migration scripts const instead of var.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var TutorialToken = artifacts.require("./TutorialToken.sol");
-var ComplexStorage = artifacts.require("./ComplexStorage.sol");
+const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const TutorialToken = artifacts.require("./TutorialToken.sol");
+const ComplexStorage = artifacts.require("./ComplexStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
