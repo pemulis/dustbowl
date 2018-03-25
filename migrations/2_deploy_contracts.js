80eb1718921e49490ef500d14e93734a31cabd0e
John Shutt <john.d.shutt@gmail.com>
2018-03-24 18:03:47 -0700

    Add WaterToken and WellsFargone to migration script.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const TutorialToken = artifacts.require("./TutorialToken.sol");
 const ComplexStorage = artifacts.require("./ComplexStorage.sol");
+const WaterToken = artifacts.require("./WaterToken.sol");
+const WellsFargone = artifacts.require("./WellsFargone.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
+  deployer.deploy(WaterToken);
+  deployer.deploy(WellsFargone);
 };
