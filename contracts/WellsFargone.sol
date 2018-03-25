c7ee0da4f42405e873adb94c4dc821dc5e68b51f
John Shutt <john.d.shutt@gmail.com>
2018-03-24 17:52:38 -0700

    update openzeppelin to its latest version and fill out a WellsFargone token minting contract

--- contracts/WellsFargone.sol
+++ contracts/WellsFargone.sol
@@ -1,1 +1,10 @@
 pragma solidity ^0.4.18;
+
+import "./WaterToken.sol";
+import 'zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
+
+contract WellsFargone is MintedCrowdsale {
+  function WellsFargone (uint256 _rate, address _wallet, MintableToken _token) public Crowdsale(_rate, _wallet, _token) {
+
+  }
+}
