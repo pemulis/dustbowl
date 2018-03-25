f46803566fd58391e983fb5116c7541a19848353
John Shutt <john.d.shutt@gmail.com>
2018-03-24 18:19:53 -0700

    Rewrite migration code to successfully deploy WaterToken and WellsFargone contracts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,23 @@ const ComplexStorage = artifacts.require("./ComplexStorage.sol");
 const WaterToken = artifacts.require("./WaterToken.sol");
 const WellsFargone = artifacts.require("./WellsFargone.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  const rate = new web3.BigNumber(1000);
+  const wallet = accounts[1];
+
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
-  deployer.deploy(WaterToken);
-  deployer.deploy(WellsFargone);
+  return deployer
+    .then(() => {
+        return deployer.deploy(WaterToken);
+    })
+    .then(() => {
+        return deployer.deploy(
+            WellsFargone,
+            rate,
+            wallet,
+            WaterToken.address
+        );
+    });
 };
