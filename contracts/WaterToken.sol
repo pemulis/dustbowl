d38d5ad4f28f667f4a812b61d6b9f5feca164620
John Shutt <john.d.shutt@gmail.com>
2018-03-24 17:33:38 -0700

    increase potential size of WaterToken supply and touch WellsFargone contract

--- contracts/WaterToken.sol
+++ contracts/WaterToken.sol
@@ -5,8 +5,8 @@ import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 contract WaterToken is MintableToken {
   string public name = "Water";
   string public symbol = "💧";
-  uint public decimals = 2;
-  uint public INITIAL_SUPPLY = 1000;
+  uint public decimals = 18;
+  uint public INITIAL_SUPPLY = 0;
 
   function WaterToken() public {
     totalSupply_ = INITIAL_SUPPLY;
