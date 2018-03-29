fed1471a7ac30d5c03da46bbb30fbbbfdc7ab2a4
John Shutt <john.d.shutt@gmail.com>
2018-03-28 17:12:12 -0700

    simplify require statements for artifacts in migration scripts, since they don't need to reference the full filepath, just the names of the contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
-const TutorialToken = artifacts.require("./TutorialToken.sol");
-const ComplexStorage = artifacts.require("./ComplexStorage.sol");
-const Water = artifacts.require("./Water.sol");
-const WellsFargone = artifacts.require("./WellsFargone.sol");
+const SimpleStorage = artifacts.require("SimpleStorage");
+const TutorialToken = artifacts.require("TutorialToken");
+const ComplexStorage = artifacts.require("ComplexStorage");
+const Water = artifacts.require("Water");
+const WellsFargone = artifacts.require("WellsFargone");
 
 module.exports = function(deployer, network, accounts) {
   const rate = new web3.BigNumber(1000);
