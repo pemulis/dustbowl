0e229d73fa29cd73d6d14f17f93cd8593c408ece
John Shutt <john.d.shutt@gmail.com>
2018-04-02 11:00:08 -0700

    update drizzle-react-components node module, delete SimpleStorage tests, and stub out a (failing) test for WellsFargone contract

---
+++ test/wellsfargone.js
@@ -0,0 +1,11 @@
+const Water = artifacts.require("Water");
+const WellsFargone = artifacts.require('WellsFargone');
+
+contract('WellsFargone', function(accounts) {
+  it("should mint 1000000000000000000000 water tokens when users send 1 ether", function() {
+    return WellsFargone.deployed().then(function(wellsFargoneInstance) {
+      web3.eth.sendTransaction({from:accounts[0].coinbase,to:wellsFargoneInstance.address,value:web3.toWei(1, "ether")});
+      return Water.deployed().totalSupply_;
+    });
+  });
+});
