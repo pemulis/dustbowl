c778bea2e47176c54861a77b3dacb9de052e65b3
John Shutt <john.d.shutt@gmail.com>
2018-04-02 18:12:20 -0700

    begin rewriting WellsFargone and Water purchase test, which is not yet passing

--- test/wellsfargone.js
+++ test/wellsfargone.js
@@ -5,7 +5,11 @@ contract('WellsFargone', function(accounts) {
   it("should mint 1000000000000000000000 water tokens when users send 1 ether", function() {
     return WellsFargone.deployed().then(function(wellsFargoneInstance) {
       web3.eth.sendTransaction({from:accounts[0],to:wellsFargoneInstance.address,value:web3.toWei(1, "ether")});
-      return Water.deployed().totalSupply_;
+      return Water.deployed();
+    }).then(function(waterInstance) {
+      return waterInstance.methods.totalSupply.call();
+    }).then(function(supply) {
+      assert.equal(supply, 1000000000000000000000);
     });
   });
 });
