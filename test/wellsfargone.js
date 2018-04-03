f995ab5d0216a023ac0a8895b6ebebc2f10ce1ec
John Shutt <john.d.shutt@gmail.com>
2018-04-02 17:38:41 -0700

    get initial WellsFargone minting test working

--- test/wellsfargone.js
+++ test/wellsfargone.js
@@ -4,7 +4,7 @@ const WellsFargone = artifacts.require('WellsFargone');
 contract('WellsFargone', function(accounts) {
   it("should mint 1000000000000000000000 water tokens when users send 1 ether", function() {
     return WellsFargone.deployed().then(function(wellsFargoneInstance) {
-      web3.eth.sendTransaction({from:accounts[0].coinbase,to:wellsFargoneInstance.address,value:web3.toWei(1, "ether")});
+      web3.eth.sendTransaction({from:accounts[0],to:wellsFargoneInstance.address,value:web3.toWei(1, "ether")});
       return Water.deployed().totalSupply_;
     });
   });
