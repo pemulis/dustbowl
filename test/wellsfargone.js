65a0ceb91f9393d7848566c40cdb41ab45cde033
John Shutt <john.d.shutt@gmail.com>
2018-04-07 13:57:37 -0700

    rewrite my test to use an async/await notation, but it is not working as expected. Shows 0 passing, but says nothing about tests failing, or acknowledging the existence of the test written in my file, which is testing if Water ownership is transferred to WellsFargone.

--- test/wellsfargone.js
+++ test/wellsfargone.js
@@ -1,8 +1,25 @@
 const Water = artifacts.require("Water");
 const WellsFargone = artifacts.require('WellsFargone');
 
+contract('WellsFargone', async(accounts) => {
+  const rate = new web3.BigNumber(1000);
+  const wallet = accounts[1];
+
+  const waterInstance = await Water.new({from: accounts[0]});
+  const wellsFargoneInstance = await WellsFargone.new(rate, wallet, Water.address);
+
+  it("should transfer ownership of Water to WellsFargone", async() => {
+    waterInstance.transferOwnership(WellsFargone.address);
+    let waterOwner = waterInstance.owner;
+    assert.equal(waterOwner, WellsFargone.address);
+  });
+});
+
+/* These tests aren't working. Going to try to rewrite async/await style. */
+
+/*
 contract('WellsFargone', function(accounts) {
-  it("should mint 1000000000000000000000 water tokens when users send 1 ether", function() {
+  it("should mint 1000000000000000000000 water tokens when user sends 1 ether", function() {
     return WellsFargone.deployed().then(function(wellsFargoneInstance) {
       web3.eth.sendTransaction({from:accounts[0],to:wellsFargoneInstance.address,value:web3.toWei(1, "ether")});
       return Water.deployed();
@@ -13,3 +30,4 @@ contract('WellsFargone', function(accounts) {
     });
   });
 });
+*/
