83d78c19fd67dcfded1884eae9ed379afe8a0da6
John Shutt <john.d.shutt@gmail.com>
2018-05-08 14:24:54 -0700

    upgrade to newest drizzle box dependencies, and update name of openzeppelin-solidity library in contracts

--- contracts/Water.sol
+++ contracts/Water.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.19;
 
-import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract Water is MintableToken {
   string public name = "Water";
--- contracts/WellsFargone.sol
+++ contracts/WellsFargone.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.19;
 
-import 'zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
+import 'openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
 
 contract WellsFargone is MintedCrowdsale {
   function WellsFargone (uint256 _rate, address _wallet, MintableToken _token) public Crowdsale(_rate, _wallet, _token) {
