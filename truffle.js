724619401cdd9898d27118cd68f7d54e25e80d33
John Shutt <john.d.shutt@gmail.com>
2018-05-09 18:05:09 -0700

    delete truffle-config.js since it's only used for Windows development, and begin stubbing out a new network configuration

--- truffle.js
+++ truffle.js
@@ -6,11 +6,25 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+    /*
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/");
+      },
+      network_id: '4',
+    },
+    development: {
+      provider: function() {
+        new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+      },
+      network_id: "*" // Match any network id
+    }
+    */
   },
   solc: {
     optimizer: {
       enabled: true,
       runs: 500
     }
-  } 
+  }
 };
