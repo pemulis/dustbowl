23d347e542624c0f8c22feb0f76d31dca3c75165
John Shutt <john.d.shutt@gmail.com>
2018-05-10 15:04:14 -0700

    change wallet address for WellsFargone contract to the account that is deploying the contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const WellsFargone = artifacts.require("WellsFargone");
 
 module.exports = function(deployer, network, accounts) {
   const rate = new web3.BigNumber(1000);
-  const wallet = accounts[1];
+  const wallet = accounts[0];
 
   deployer.deploy(Water).then(function() {
     return deployer.deploy(WellsFargone, rate, wallet, Water.address);
