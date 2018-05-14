d6047e246e5c2e6678c67c1968ffb6d6c17ff5f7
John Shutt <john.d.shutt@gmail.com>
2018-05-14 11:37:56 -0700

    update Rinkeby address for Wells Fargone in user interface

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -20,7 +20,7 @@ class Home extends Component {
             <p>Buy precious life-giving Water from ghosts in exchange for Ether, which they can use on the ethereal plane.</p>
             <p><strong>Total Supply</strong>: <ContractData contract="Water" method="totalSupply" /> <ContractData contract="Water" method="symbol" /></p>
             <p><strong>My Balance</strong>: <ContractData contract="Water" method="balanceOf" methodArgs={[this.props.accounts[0]]} /></p>
-            <h4>Buy Water on Rinkeby by transferring Ether to the WellsFargone address: 0x51cfb1bdcde3f57437aa863d4470502cad68f6b2</h4>
+            <h4>Buy Water on Rinkeby by transferring Ether to the WellsFargone address: 0xd4a8e090a71ad937c49e1b53d4812f4e518c5214</h4>
             <br/><br/>
           </div>
 
